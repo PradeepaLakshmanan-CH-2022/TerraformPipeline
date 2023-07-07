916ae8498436961a7bcfe754a32e05cb6c6f2a33
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-07 18:37:49 +0530

    pair

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "pipelineinstance" {
   ami           = "ami-04132f301c3e4f138"
   instance_type = "t2.micro"
-  key_name = "Consolepair"
+  key_name = "NewEC2pair"
   
 
     
