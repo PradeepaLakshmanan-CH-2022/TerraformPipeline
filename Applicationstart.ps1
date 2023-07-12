5e2cc1b009b0046e9050fd3e54b12e5c7b2051f2
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-12 19:12:34 +0530

    output

--- Applicationstart.ps1
+++ Applicationstart.ps1
@@ -10,7 +10,7 @@ Remove-Item -Path $consoleAppPath\* -Recurse -Force
 dotnet publish -c Release -o $consoleAppPath
 
 # Specify the path for the output file
-$outputFilePath = "C:\Outputfile\Output.txt"
+$outputFilePath = "C:\outputfile\output.txt"
 
 # Run the console application and redirect the output to a file
 Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath, "--", ">", $outputFilePath -NoNewWindow -Wait
